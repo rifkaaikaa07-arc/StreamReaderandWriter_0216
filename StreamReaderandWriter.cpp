f62fa7b804ab3ddededa0cc3dac1b5e7a11a22db
rifkaaikaa07-arc <rifkaaikaa07@gmail.com>
2026-06-01 19:09:07 +0700

    menmabahakan file jika kondisi true

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -33,4 +33,17 @@ int main()
     ifstream infile;
     //menunjuk ke sebuah file
     infile.open("contohfile.txt");
+
+    cout << endl << ">=Membuka dan membaca file" << endl;
+    //kondisi jika file ada
+    if (infile.is_open())
+    {
+        //perulangan untuk memunculkan setiap baris
+        while (getline(infile, baris)) {
+            cout << baris << endl;
+        }
+        //menutup file jika telah selesai
+        infile.close();
+    }
+
 }
