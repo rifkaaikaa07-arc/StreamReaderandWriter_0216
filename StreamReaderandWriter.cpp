056087e0072692be5dd19844f97cfef7492695f6
rifkaaikaa07-arc <rifkaaikaa07@gmail.com>
2026-06-01 19:01:52 +0700

    menambahkan variabel

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+
 using namespace std;
 
 int main()
 {
+    //deklarasi variabel baris untuk menyimpan data string
+    string baris;
 }
