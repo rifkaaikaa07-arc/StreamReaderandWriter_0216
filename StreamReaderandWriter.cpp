8e3638bd430b4e34e03da5df762bf0ed3b72d36f
rifkaaikaa07-arc <rifkaaikaa07@gmail.com>
2026-06-01 19:06:00 +0700

    menambahkan kondisi false dlam while

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -21,5 +21,7 @@ int main()
         cout << "- ";
         //mendapat setiap karakter dalam satu baris
         getline(cin, baris);
+        //kondisi false jika baris hanya berisi karakter q
+        if (baris == "q") break;
     }
 }
