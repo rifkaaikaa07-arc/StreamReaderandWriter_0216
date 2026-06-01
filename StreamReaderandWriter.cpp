b76541f6d59649e7e6bb925a410366654bade3b2
rifkaaikaa07-arc <rifkaaikaa07@gmail.com>
2026-06-01 19:04:07 +0700

    membuat loop while

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -15,4 +15,8 @@ int main()
     outfile.open("contohfile.txt");
 
     cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    //unlimited loop untuk menulis file
+    while (true) {
+    }
 }
