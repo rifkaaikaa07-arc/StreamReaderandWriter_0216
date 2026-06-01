b8eceaaae16ccefc0b1541525abb6c5dd21ac45a
rifkaaikaa07-arc <rifkaaikaa07@gmail.com>
2026-06-01 19:11:45 +0700

    membuta variabel baris dan nama

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -6,4 +6,31 @@ using namespace std;
 
 int main()
 {
+    //deklarasi variabel baris dan NamaFile
+    string baris, NamaFile;
+
+    //membuat input untuk menangkap nama file
+    cout << "Masukkan nama file : ";
+    cin >> NamaFile;
+
+    //membuat file dalam mode menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file dengan ekstensi txt
+    outfile.open(NamaFile + ".txt", ios::out);
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    //looping untuk menulis isi file
+    while (true)
+    {
+        cout << "- ";
+        //mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //kondisi jika baris hanya berisi huruf q
+        if (baris == "q") break;
+        //menulis dan memasukkan nilai dari baris ke dalam file
+        outfile << baris << endl;
+    }
+    //menutup file apabila telah selesai di tulis
+    outfile.close();
 }
