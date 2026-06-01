92dc778fd45a7e07f84bc2a2fb39d7b3a9535071
rifkaaikaa07-arc <rifkaaikaa07@gmail.com>
2026-06-01 19:05:18 +0700

    menambahkan getline ke dalam while

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -18,5 +18,8 @@ int main()
 
     //unlimited loop untuk menulis file
     while (true) {
+        cout << "- ";
+        //mendapat setiap karakter dalam satu baris
+        getline(cin, baris);
     }
 }
